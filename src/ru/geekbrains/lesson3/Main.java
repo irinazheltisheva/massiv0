e8ca96fe4563b6106477be6d531ac5cb2dbe1eb6
Irina <70594426+irinazheltisheva@users.noreply.github.com>
2021-05-25 09:14:49 +0300

    Initial commit

---
+++ src/ru/geekbrains/lesson3/Main.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.lesson3;
+
+import java.util.Arrays;
+
+public class Main {
+    public static int i;
+
+    public static void fillArray(int[] arr) {
+
+    }
+    @SuppressWarnings(value = "unused")
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+
+        int[] arr = new int[100];
+        for (i = 0; i < arr.length; i++) {
+            final int i1 = 100;
+            final int i2 = 1;
+            // 2 3 4 5 6 7 8 & hellip; 100
+
+
+        }
+    }
+}
+
